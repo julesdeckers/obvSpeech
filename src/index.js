7d7792b178dbc53872d30654d5830166a0603f37
julesdeckers <jules.deckers@gmail.com>
2019-06-12 11:09:12 +0200

    added delay after recognition

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+if (!("webkitSpeechRecognition" in window)) {
+  alert(
+    "Speech recognition API niet gevonden, gelieve Google Chrome te gebruiken"
+  );
+}
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -11,6 +16,13 @@ words.appendChild(p);
 const $title = document.querySelector(`.title`);
 const $sub = document.querySelector(`.subtext`);
 
+const funcDelay = 500;
+
+const resetContainer = () => {
+  $wordsContainer.classList.add("hide");
+  p.textContent = "";
+};
+
 const detectComfy = () => {
   recognition.addEventListener("result", e => {
     const transcript = Array.from(e.results)
@@ -18,11 +30,17 @@ const detectComfy = () => {
       .map(result => result.transcript)
       .join("");
 
-    p.textContent = transcript;
+    if (p.textContent !== "") {
+      p.textContent = transcript;
+    } else {
+      $wordsContainer.classList.remove("hide");
+      p.textContent = transcript;
+    }
     if (transcript.includes("zit gemakkelijk")) {
       recognition.abort();
       // $wordsContainer.classList.add("hide");
-      comfyComplete();
+      setTimeout(comfyComplete, funcDelay);
+      // comfyComplete();
     }
   });
 
@@ -30,17 +48,10 @@ const detectComfy = () => {
   recognition.start();
 };
 
-const handleEnter = e => {
-  console.log("key pressed");
-  if (e.keyCode === 13) {
-    console.log("verhaal opgeslagen");
-  }
-};
-
 const comfyComplete = () => {
+  resetContainer();
   $title.textContent = "Goed zo! Zoals je ziet moet je goed articuleren.";
   $sub.textContent = "Zeg ‘Ik snap het’ om verder te gaan.";
-  document.removeEventListener("keyup", handleEnter);
   detectUnderstood();
 };
 
@@ -51,10 +62,16 @@ const detectUnderstood = () => {
       .map(result => result.transcript)
       .join("");
 
-    p.textContent = transcript;
+    if (p.textContent !== "") {
+      p.textContent = transcript;
+    } else {
+      $wordsContainer.classList.remove("hide");
+      p.textContent = transcript;
+    }
+    // p.textContent = transcript;
     if (transcript.includes("snap het")) {
       recognition.abort();
-      understoodComplete();
+      setTimeout(understoodComplete, funcDelay);
     }
   });
 
@@ -63,6 +80,7 @@ const detectUnderstood = () => {
 };
 
 const understoodComplete = () => {
+  resetContainer();
   $title.textContent =
     "De voorstelling die je zonet gezien hebt, komt uit de opera ‘La Bohème’ en is geschreven, na een bijna-dood-ervaring van de schrijver. Heb jij al eens een bijna dood ervaring gehad?";
   $sub.textContent = "‘Ja, en ik wil deze vertellen’ of ‘Nee, nog nooit’";
@@ -76,14 +94,19 @@ const detectStory = () => {
       .map(result => result.transcript)
       .join("");
 
-    p.textContent = transcript;
+    if (p.textContent !== "") {
+      p.textContent = transcript;
+    } else {
+      $wordsContainer.classList.remove("hide");
+      p.textContent = transcript;
+    }
     if (transcript.includes("ik wil deze vertellen")) {
       recognition.abort();
-      storyYes();
+      setTimeout(storyYes, funcDelay);
     }
     if (transcript.includes("nog nooit")) {
       recognition.abort();
-      storyNo();
+      setTimeout(storyNo, funcDelay);
     }
   });
 
@@ -92,8 +115,9 @@ const detectStory = () => {
 };
 
 const storyYes = () => {
+  resetContainer();
   $title.textContent =
-    "Denk even rustig na, en begin aub met spreken wanneer je wil. Wanneer je klaar bent zeg je: ‘en dit was mijn verhaal’ of druk je op enter.";
+    "Denk even rustig na, en begin aub met spreken wanneer je wil. Wanneer je klaar bent zeg je: ‘en dit was mijn verhaal’.";
   $sub.textContent = "";
   recognition.addEventListener("speechstart", recordStory);
 };
@@ -101,11 +125,8 @@ const storyYes = () => {
 const storyNo = () => {};
 
 const recordStory = () => {
-  $wordsContainer.classList.remove("words--container");
-  $wordsContainer.classList.add("words--container2");
   recognition.removeEventListener("speechstart", recordStory);
   recognition.addEventListener("result", e => {
-    document.addEventListener("keyup", handleEnter);
     const transcript = Array.from(e.results)
       .map(result => result[0])
       .map(result => result.transcript)
@@ -115,6 +136,7 @@ const recordStory = () => {
     if (transcript.includes("dit was mijn verhaal")) {
       recognition.stop();
       console.log("verhaal opgeslagen");
+      // storyRecorded(transcript);
       storyRecorded(transcript);
     }
 
@@ -129,7 +151,7 @@ const recordStory = () => {
 };
 
 const storyRecorded = transcript => {
-  console.log("transcript");
+  console.log(transcript);
 };
 
 const init = () => {
